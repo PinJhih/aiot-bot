0134398413bcd0c78fcdecc4833a0976a02d8ef8
PinJhih <isaacwang29@gmail.com>
2024-05-24 22:22:42 +0800

    feat: Use dot env

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
-import discord
+import os
 
-TOKEN = "YOUR_DISCORD_BOT_TOKEN"
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('BOT_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
