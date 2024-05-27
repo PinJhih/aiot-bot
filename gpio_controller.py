624846e7d10e6e96b577c8a00057427350f2726b
PinJhih <isaacwang29@gmail.com>
2024-05-27 15:04:06 +0800

    feat: Control arduino

--- gpio_controller.py
+++ gpio_controller.py
@@ -1,10 +1,25 @@
+import time
+
+import serial
+
 from logger import logger
+
+arduino_port = "COM4"
+baud_rate = 9600
+
+ser = serial.Serial(arduino_port, baud_rate)
+time.sleep(2)
+
+
+def send_command(command):
+    ser.write(command.encode())
 
 
 def turn_on_classroom_light():
     """
     將教室的電燈打開
     """
+    send_command("0")
     logger.info("Turn on the light in the classroom")
     return {"succeed": True}
 
@@ -13,6 +28,7 @@ def turn_on_lab_light():
     """
     將實驗室的電燈打開
     """
+    send_command("1")
     logger.info("Turn on the light in the lab")
     return {"succeed": True}
 
@@ -21,6 +37,7 @@ def turn_off_classroom_light():
     """
     將教室的電燈關閉
     """
+    send_command("2")
     logger.info("Turn off the light in the classroom")
     return {"succeed": True}
 
@@ -29,5 +46,6 @@ def turn_off_lab_light():
     """
     將實驗室的電燈關閉
     """
+    send_command("3")
     logger.info("Turn off the light in the lab")
     return {"succeed": True}
